5ef892d8c140733a5902c78674edf236903dc60f
HypeLevels <gmest0re7@gmail.com>
2021-06-04 02:28:30 +0100

    fix(CommandHandler/emit): made the event camelCase

--- src/struct/commands/CommandHandler.js
+++ src/struct/commands/CommandHandler.js
@@ -1015,7 +1015,8 @@ class CommandHandler extends AkairoHandler {
 			message.channel.startTyping();
 		}
 		if (command.onlyNsfw && !message.channel.nsfw) {
-			return this.emit("notNSFW", message, command)
+			return this.emit("notNsfw", message, command)
+			
 		}
 		try {
 			this.emit(CommandHandlerEvents.COMMAND_STARTED, message, command, args);
