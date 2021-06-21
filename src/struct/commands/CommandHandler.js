c8d0d8458e1d8d313a05eba026f928d6d6519c07
FC5570 <68158483+FC5570@users.noreply.github.com>
2021-06-21 12:37:05 +0530

    Emits the commandBlocked event for non nsfw channels (#26)
    * Emit the commandBlocked for nsfw commands
    
    * Remove COMMAND_LOCKED_NSFW

--- src/struct/commands/CommandHandler.js
+++ src/struct/commands/CommandHandler.js
@@ -1122,7 +1122,7 @@ class CommandHandler extends AkairoHandler {
 		}
 
 		if (command.onlyNsfw && !message.channel.nsfw) {
-			this.emit(CommandHandlerEvents.COMMAND_LOCKED_NSFW, message, command);
+			this.emit(CommandHandlerEvents.COMMAND_BLOCKED, message, command, 'notNsfw');
 			return;
 		}
 
